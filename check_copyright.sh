2188acfe3eb36eae49fea791b933eb8634e58895
Tim Hatch <thatch@fb.com>
2020-09-25 08:02:36 -0700

    Ensure the copyright checking script has proper copyright

--- check_copyright.sh
+++ check_copyright.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 set -eu
 
 die() { echo "$1"; exit 1; }
 
 while read filename; do \
-  grep -q "Copyright (c) Facebook" "$filename" ||
+  grep -q "^# Copyright (c) Facebook" "$filename" ||
     die "Missing copyright in $filename"
-done < <( git ls-tree -r --name-only HEAD | grep ".py$" )
+done < <( git ls-tree -r --name-only HEAD | grep "\(.py\|\.sh\)$" )
 
