0a771055b04051b53b28091870ec5644bf767deb
John Reese <john@noswap.com>
2022-01-25 19:04:51 -0800

    Facebook -> Meta

--- check_copyright.sh
+++ check_copyright.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (c) Facebook, Inc. and its affiliates.
+# Copyright (c) Meta Platforms, Inc. and affiliates.
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
@@ -10,7 +10,7 @@ set -eu
 die() { echo "$1"; exit 1; }
 
 while read filename; do \
-  grep -q "^# Copyright (c) Facebook" "$filename" ||
+  grep -q "^# Copyright (c) Meta Platforms, Inc. and affiliates." "$filename" ||
     die "Missing copyright in $filename"
 done < <( git ls-tree -r --name-only HEAD | grep "\(.py\|\.sh\)$" )
 
